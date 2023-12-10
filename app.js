75b59b4719538ffe954cd1590b8807308386d591
ienyh <17513366907@163.com>
2023-12-10 14:03:21 +0800

    chore: routes

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const PORT = process.env.PORT || 5178
 async function register(app) {
   const lib = `${__dirname}/lib`
   const dir = await fs.readdir(lib)
+  const routes = []
   dir.forEach(filename => {
-    app.get(filename.replace(/\.js$/, ''), require(`${lib}/${filename}`))
+    const route = filename.replace(/\.js$/, '')
+    routes.push(route)
+    app.get(`/${route}`, require(`${lib}/${filename}`))
+  })
+  app.get('/', (req, res) => {
+    res.end(routes.map(r => `/${r}\n`).join())
   })
 }
 
